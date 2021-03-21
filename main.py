b3213c8a4c5afb01dd9bebcf35ec5a893cb845d1
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-03-21 11:21:17 +0600

    new

--- main.py
+++ main.py
@@ -10,6 +10,7 @@
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
 
 
 
@@ -26,9 +27,17 @@ waiting_for_page = 10
 
 driver.get("https://engine.presearch.org")
 time.sleep(2)
-print(input("Enter your Username and Password Menually then enter 1: "))
-driver.get("https://presearch.org")
-print(input("Enter your Username and Password Menually then enter 1: "))
+# print(input("Enter your Username and Password Menually then enter 1: "))
+
+#TODO: Open New Tab
+
+# driver.find_element_by_tag_name('body').send_keys(Keys.COMMAND + 't')
+# driver.execute_script("window.open('https://presearch.org');")
+# driver.execute_script("window.open('https://engine.presearch.org');")
+# driver.close()
+# driver.get("https://engine.presearch.org")
+
+# print(input("Enter your Username and Password Menually then enter 1: "))
 driver.find_element_by_id("search").send_keys("username")
 print(input("Enter your Username and Password Menually then enter 1: "))
 time.sleep(1)
