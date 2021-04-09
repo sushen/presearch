fb915f7f6acef3b574ffc9c2af85d4252cd2942b
Md Jawad Hossain <jawadsgs3@gmail.com>
2021-04-09 10:03:04 +0600

    Fully Upgrade main.py

--- main.py
+++ main.py
@@ -1,86 +1,87 @@
-#   Copyright (c) 2021.
-#   Version : 1.0.2
-#   Script Author : Sushen Biswas
-#
-#   Sushen Biswas Github Link : https://github.com/sushen
-#
-#   !/usr/bin/env python
-#   coding: utf-8
-
 from selenium import webdriver
 import time
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import os
 import random
 
-# TODO: Make a Long List of keyword
-all_keys = ["username", "password", "country", "income", "funny"]
-random_time = [6, 10, 20, 5, 15, 9, 25, 14, 9, 18, 23, 10]
+all_keys = ["make money fast today",
+"how to make money online for beginners",
+"how to make money online for free",
+"how to make quick money in one day",
+"creative ways to make money",
+"real ways to make money from home",
+"ideas to make money",
+"how to make money in life",
+"most viewed youtube video in 24 hours",
+"what is the most viewed video on tiktok",
+"most viewed youtube video 2019",
+"most viewed youtube channel",
+"most viewed youtube video not music",
+"most viewed video on youtube in india",
+"top 50 most viewed youtube videos of all time",
+"Md Jawad Hossain",
+"Tech Teach Pro",
+"fanyconda",
+"Hollow Block Bd"]
+rand_time= [5, 9, 16, 6, 12, 10, 8, 7, 11, 13, 14, 15]
+email = os.environ.get('email')
+password = os.environ.get('password')
 
 chrome_options = Options()
 chrome_options.add_argument("--user-data-dir=chrome-data")
 chrome_options.add_argument("--start-maximized")
-# chrome_options.add_argument("--incognito")
-
 driver = webdriver.Chrome("./chromedriver.exe", chrome_options=chrome_options)
 chrome_options.add_argument("user-data-dir=chrome-data")
-driver.implicitly_wait(25)  # seconds
-# What will be searched
-
 actions = ActionChains(driver)
-
-# Time waiting for page
-waiting_for_page = 10
-
-driver.get("https://engine.presearch.org")
-time.sleep(2)
-
-# TODO: Make a login strong system in environment veriable
-print(input("Enter your Username and Password Menually then enter 1: "))
-driver.get("https://presearch.org")
-# print(input("Enter your Username and Password Menually then enter 1: "))
-driver.find_element_by_id("search").send_keys(random.choice(all_keys))
-driver.find_element_by_id("search").send_keys(Keys.ENTER)
-# print(input("Enter your Username and Password Menually then enter 1: "))
-time.sleep(2)
-
-actions.send_keys(Keys.TAB * 10)
-search_key = random.choice(all_keys)
-actions.send_keys(search_key)
-actions.send_keys(Keys.ENTER)
-actions.perform()
-time.sleep(10)
-prev_search_key = search_key
-
-while True:
-    for i in range(len(prev_search_key)):
-        actions.send_keys(Keys.BACK_SPACE)
-        while True:
-            for v in range(len(random_time)):
-                actions.send_keys(Keys.BACK_SPACE * 20)
-                search_key = random.choice(all_keys)
-                print(search_key)
-                actions.send_keys(search_key)
-                actions.send_keys(Keys.ENTER)
-                actions.perform()
-                prev_search_key = search_key
-                time.sleep(random_time[v])
-
-# TODO: Click the search resust to make it more human
+driver.get('https://engine.presearch.org/')
 
 
-# driver.find_element_by_id("token-animation").click()
-# print(driver.find_element_by_xpath("//button[@type='submit']"))
-# driver.find_element_by_xpath("//input[@type='submit']").click();
-# print(input("Enter your Username and Password Menually then enter 1: "))
 
-# driver.execute_script("window.open('https://engine.presearch.org');")
-# driver.close()
-# driver.get("https://presearch.org")
-# driver.find_element_by_id("search").send_keys("username")
-# driver.find_element_by_id("search").send_keys(Keys.ENTER)
+def login():
+    window_before = driver.window_handles[0]
+    driver.find_element_by_xpath('//*[@id="Home"]/div[1]/div/div[2]/a').click() #Register or Login
+    window_after = driver.window_handles[1]
+    driver.switch_to_window(window_after)
+    driver.find_element_by_name('email').send_keys(email) #email
+    driver.find_element_by_name('password').send_keys(password) #password
+    driver.find_element_by_name('remember').click() #Remember Me
+    input('Verify the reCAPTCHS and press 1 :  ')
+    driver.find_element_by_xpath('//*[@id="login-form"]/form/div[3]/div[3]/button').click()
+    time.sleep(2)
+    driver.switch_to_window(window_before)
+    time.sleep(2)
+    driver.get('https://engine.presearch.org/')
+    time.sleep(3)
+    driver.set_page_load_timeout(7)
 
-time.sleep(5)
 
-driver.quit()
+def search1():
+    random_time = random.choice(rand_time)
+    actions.send_keys(Keys.TAB * 10)
+    search_key = random.choice(all_keys)
+    actions.send_keys(search_key, Keys.ENTER)
+    actions.perform()
+    time.sleep(random_time)
+    prev_search_key = search_key
+
+    while True:
+        for i in range(len(prev_search_key)):
+            actions.send_keys(Keys.BACK_SPACE)
+
+        time.sleep(4)
+        search_key = random.choice(all_keys)
+        print(search_key)
+        actions.send_keys(search_key, Keys.ENTER)
+        actions.perform()
+        prev_search_key = search_key
+        time.sleep(4)
+
+try:
+    login()
+
+except:
+    pass
+
+search1()
